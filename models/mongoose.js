f2dc2c7dbc42e6efa963d73c1253765f168c9024
Jakub <jkb.olbrycht@gmail.com>
2022-04-25 20:59:01 +0200

    Local Library first commit

---
+++ models/mongoose.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+//Define a schema
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+  name: String,
+  binary: Buffer,
+  living: Boolean,
+  updated: { type: Date, default: Date.now() },
+  age: { type: Number, min: 18, max: 65, required: true },
+  mixed: Schema.Types.Mixed,
+  _someId: Schema.Types.ObjectId,
+  array: [],
+  ofString: [String], // You can also have an array of each of the other types too.
+  nested: { stuff: { type: String, lowercase: true, trim: true } },
+});
+
+const SomeModel = mongoose.model("SomeModel", SomeModelSchema);
