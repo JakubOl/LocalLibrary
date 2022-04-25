07f773a70406ca389d7ef6ea1f626bbd29708f84
Jakub <jkb.olbrycht@gmail.com>
2022-04-25 23:40:18 +0200

    Library App finished

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ const MONGO_PASSWORD = process.env.PASSWORD;
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const catalogRouter = require("./routes/catalog");
-
+const compression = require("compression");
+const helmet = require("helmet");
 const app = express();
 
 const mongoose = require("mongoose");
-const mongoDB = `mongodb+srv://jakub:jakub@cluster0.yonlg.mongodb.net/local_library?retryWrites=true&w=majority`;
+const mongoDB = `mongodb+srv://jakub:${MONGO_PASSWORD}@cluster0.yonlg.mongodb.net/local_library?retryWrites=true&w=majority`;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
@@ -21,10 +22,12 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+app.use(helmet());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
