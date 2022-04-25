309a4e9d7bc8013e7fa0c2133350020a8bdaa13b
Jakub <jkb.olbrycht@gmail.com>
2022-04-25 23:43:57 +0200

    Changed mongodb link

--- app.js
+++ app.js
@@ -12,8 +12,10 @@ const compression = require("compression");
 const helmet = require("helmet");
 const app = express();
 
+const dev_db_url = `mongodb+srv://jakub:${MONGO_PASSWORD}@cluster0.yonlg.mongodb.net/local_library?retryWrites=true&w=majority`;
+
 const mongoose = require("mongoose");
-const mongoDB = `mongodb+srv://jakub:${MONGO_PASSWORD}@cluster0.yonlg.mongodb.net/local_library?retryWrites=true&w=majority`;
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
